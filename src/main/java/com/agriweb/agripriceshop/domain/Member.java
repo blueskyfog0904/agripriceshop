c807ac3d5beb64da571d545eea1dbe9ad9734498
DESKTOP-90K0DJ5\User <khkim@brainednet.com>
2023-05-12 13:19:27 +0900

    commit

--- src/main/java/com/agriweb/agripriceshop/domain/Member.java
+++ src/main/java/com/agriweb/agripriceshop/domain/Member.java
@@ -1,4 +1,41 @@
 package com.agriweb.agripriceshop.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter @Setter
 public class Member {
+
+    @Id @GeneratedValue
+    @Column(name="member_id")
+    private Long id;
+
+    private String loginId;
+
+    private String pw;
+
+    private String userName;
+
+    private LocalDate birthdate;
+
+    private String gender;
+
+    private String tel;
+
+    private Address addr;
+
+    private String email;
+
+    private List<Order> orders;
+
+    private List<Board> boards;
+
+
 }
