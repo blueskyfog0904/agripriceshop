403c1216de398a363de6e242e5f2fc3d7f26cc12
blueskyfog0904 <blueskyfog@gmail.com>
2023-06-06 21:54:47 +0900

    spring security & jwt 구현 중

---
+++ src/main/java/com/agriweb/agripriceshop/jwt/JwtAccessDeniedHandler.java
@@ -0,0 +1,24 @@
+package com.agriweb.agripriceshop.jwt;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+/**
+ * AccessDeniedHandler
+ *
+ * AuthenticationEntryPoint와 달리 AccessDeniedHandler는
+ * 유저 정보는 있으나, 엑세스 권한이 없는 경우 동작하는 친구이다.
+ */
+@Component
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_FORBIDDEN);
+    }
+}
---
+++ src/main/java/com/agriweb/agripriceshop/jwt/JwtTokenFilter.java
@@ -0,0 +1,42 @@
+package com.agriweb.agripriceshop.jwt;
+
+import com.agriweb.agripriceshop.exception.CustomException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtTokenFilter extends OncePerRequestFilter {
+    private JwtTokenProvider jwtTokenProvider;
+
+    public JwtTokenFilter(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+        String token = jwtTokenProvider.resolveToken(request);
+        try {
+            if (token != null && jwtTokenProvider.validateToken(token)) {
+                Authentication auth = jwtTokenProvider.getAuthentication(token);
+                // 정상 토큰이면 토큰을 통해 생성한 Authentication 객체를 SecurityContext에 저장
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        } catch (CustomException e) {
+            SecurityContextHolder.clearContext();
+            response.sendError(e.getHttpStatus().value(), e.getMessage());
+            return;
+        }
+
+        filterChain.doFilter(request, response); // 다음 필터 체인 실행
+    }
+}
