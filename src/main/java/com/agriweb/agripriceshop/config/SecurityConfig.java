33cc671a86088dec16c2df96241e08e15055f10a
DESKTOP-90K0DJ5\User <khkim@brainednet.com>
2023-06-07 17:06:56 +0900

    spring security + jwt

--- src/main/java/com/agriweb/agripriceshop/config/SecurityConfig.java
+++ src/main/java/com/agriweb/agripriceshop/config/SecurityConfig.java
@@ -1,121 +1,42 @@
 package com.agriweb.agripriceshop.config;
 
-import com.agriweb.agripriceshop.config.handler.Http403Handler;
-import com.agriweb.agripriceshop.domain.Member;
-import com.agriweb.agripriceshop.repository.MemberRepository;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.logging.Log;
-import org.slf4j.Logger;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import com.agriweb.agripriceshop.jwt.JwtAuthenticationFilter;
+import com.agriweb.agripriceshop.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
-import org.springframework.security.core.parameters.P;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.AccessDeniedHandler;
-import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import java.io.IOException;
-
-@Slf4j
 @Configuration
-@EnableWebSecurity(debug = true) // 운영환경에서는 꺼야하며, 개발환경에서 spring security가 잘 돌아가는지 자세한 로그 확인을 위해 켜놓음
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() { // 인증 안해도 되게 해주는 기능
-        return new WebSecurityCustomizer() {
-            @Override
-            public void customize(WebSecurity web) {
-                web.ignoring().requestMatchers("/favicon.ico")
-                        .requestMatchers(new AntPathRequestMatcher("/h2-console/**"));
-
-
-
-            }
-        };
-    }
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        return http
-                .authorizeHttpRequests()
-                .requestMatchers("/auth/login").permitAll()
-                .requestMatchers("/auth/signup").permitAll()
-                .requestMatchers("/user").hasAnyRole("USER", "ADMIN")
-//                .requestMatchers("/admin")
-//                .access(new WebExpressionAuthorizationManager("hasRole('ADMIN') AND hasAuthority('WRITE)"))
-                .anyRequest().authenticated()
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .formLogin()
-                    .loginPage("/auth/login")
-                    .loginProcessingUrl("/auth/login")
-                    .usernameParameter("username")
-                    .passwordParameter("password")
-                    .defaultSuccessUrl("/")
+                .authorizeRequests()
+                .requestMatchers("/**").permitAll()
                 .and()
-                .exceptionHandling(e -> {
-                    e.accessDeniedHandler(new Http403Handler());
-                })
-                .rememberMe(rm -> rm.rememberMeParameter("remember")
-                    .alwaysRemember(false)
-                    .tokenValiditySeconds(2592000)
-                )
-                .csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable())
-                .build();
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
     }
-
-    @Bean
-    public UserDetailsService userDetailsService(MemberRepository memberRepository) {
-        return new UserDetailsService() {
-            @Override
-            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-                Member member = memberRepository.findOneByLoginId(username);
-                if (member == null) {
-                    throw new UsernameNotFoundException("User not found with username: " + username);
-                }
-                return new UserPrincipal(member);
-            }
-        };
-
-    }
-//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-//        UserDetails user = User.withUsername("hodolman")
-//                .password("1234")
-//                .roles("ADMIN")
-//                .build();
-//        manager.createUser(user);
-//        return manager;
-//    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new SCryptPasswordEncoder(
-                16,
-                8,
-                1,
-                32,
-                64);
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
-
 
 }
