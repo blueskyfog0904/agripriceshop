425bcc69fe24d3bd16f924cd98681b4c776707ab
DESKTOP-90K0DJ5\User <khkim@brainednet.com>
2023-06-08 13:55:15 +0900

    스프링 시큐리티 CORS 설정- https://agripriceshop.vercel.app/ 추가 재업로드

--- src/main/java/com/agriweb/agripriceshop/config/WebSecurityConfigurerAdapter.java
+++ src/main/java/com/agriweb/agripriceshop/config/WebSecurityConfigurerAdapter.java
@@ -1,4 +1,31 @@
 package com.agriweb.agripriceshop.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@EnableWebSecurity
+@Configuration
 public class WebSecurityConfigurerAdapter {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(List.of("http://localhost:3000", "https://agripriceshop.vercel.app/"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setExposedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+
 }
